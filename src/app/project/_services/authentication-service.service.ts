586b03c916b5318b35b7f213636592bbf0627e6a
dev.darshana991 <dev.darshana92@gmail.com>
2021-02-21 21:32:51 +0530

    minor

--- src/app/project/_services/authentication-service.service.ts
+++ src/app/project/_services/authentication-service.service.ts
@@ -4,28 +4,41 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from '../_models/user';
 import {HttpClient } from '@angular/common/http'
 import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationServiceService {
 
-  private userSubject!: BehaviorSubject<User>;
-  public user!: Observable<User>;
-  public baseUrl!: string;
+  private userSubject: BehaviorSubject<User>;
+  public user: Observable<User>;
+  public baseUrl: string;
 
   constructor(
     private route: Router,
     private http: HttpClient
   ) { 
-   this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.get('user')));
-   this.user = this.userSubject.asObservable();
-   this.baseUrl = environment.baseUrl;
+    this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('user')));
+    this.user = this.userSubject.asObservable();
+    this.baseUrl = environment.baseUrl;
   }
 
-  login(userName: string , password: string){
-
-     return this.http.post<any>(`${this.baseUrl}/users/auth` , {userName , password}) ;
-
-  }
+  login(username: string, password: string) {
+    return this.http.post<any>(`${environment.baseUrl}/users/authenticate`, { username, password })
+        .pipe(map(user => {
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem('user', JSON.stringify(user));
+            this.userSubject.next(user);
+            return user;
+        }));
+}
+logout() {
+  // remove user from local storage to log user out
+  localStorage.removeItem('user');
+  //this.userSubject.next();
+  this.route.navigate(['/login']);
+}
+  
+  
 }
